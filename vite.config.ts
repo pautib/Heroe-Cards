b95561686a06177c0fe6a2594928bd061db67018
pautib <pau.torresibravo@igt.com>
2025-01-18 16:03:49 +0100

    Set dist as build folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   esbuild: {
     jsxInject: `import React from 'react'`
+  },
+  build: {
+    outDir: 'dist'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
